f2a45512c533abf655dc8e4b2437fab8f8b5f794
Adriel Alejandro Aliaga Benavides <aaliagab85@gmail.com>
2022-10-02 06:40:17 -0400

    Initial commit

---
+++ src/main/java/com/ms/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.ms.springboot.app.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringbootServicioEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/springboot/app/eureka/SpringbootServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.springboot.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
